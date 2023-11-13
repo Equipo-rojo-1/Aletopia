9a77c667d91168480187f215daa642d558ff6218
Luis Mata <luisema31@gmail.com>
2023-11-12 20:39:50 -0400

    test: Adding unit tests for app.controller with a mock app.service

--- apps/ticket-generator-api/src/app/test/unit/app.controller.spec.ts
+++ apps/ticket-generator-api/src/app/test/unit/app.controller.spec.ts
@@ -2,21 +2,27 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { AppController } from '../../app.controller';
 import { AppService } from '../../app.service';
+import { qrCodeStub, ticketStub } from '../stubs';
+
+jest.mock('../../app.service');
 
 describe('AppController', () => {
   let app: TestingModule;
+  let appController: AppController;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     app = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
     }).compile();
+
+    appController = app.get<AppController>(AppController);
+    jest.clearAllMocks();
   });
 
-  /* describe('getData', () => {
-    it('should return "Hello API"', () => {
-      const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Hello API' });
+  describe('generate Ticket', () => {
+    it('should return a qr code on svg format', async () => {
+      expect(await appController.generateQrTicket(ticketStub)).toBe(qrCodeStub);
     });
-  }); */
+  });
 });
