4e12511b52b033452a1b6c50a2ac4672575918b8
Fernando Guzman <fernandeitorc@gmail.com>
2023-11-14 15:18:57 -0400

    rename app.tsx to App.tsx and reorganization

--- apps/web-zoo-catalog/src/App.tsx
+++ apps/web-zoo-catalog/src/App.tsx
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
 // components
-import Table from './../components/pure/Table';
-import ZoneButton from './../components/pure/ZoneButton';
-import AnimalCard from './../components/pure/AnimalCard';
-// json
-import { Habitat, ZoneName } from './../utils/types';
+import Table from './components/pure/Table';
+import ZoneButton from './components/pure/ZoneButton';
+import AnimalCard from './components/pure/AnimalCard';
+// json`
+import { Habitat, ZoneName } from './utils/types';
 // css
 import './App.css';
 // assets
-import zones from './../zonas.json';
-import parrotColor from './../assets/img/parrot-color.svg';
-import parrotNotColor from './../assets/img/parrot-not-color.svg';
-import dolphinColor from './../assets/img/dolphin-color.svg';
-import dolphinNotColor from './../assets/img/dolphin-not-color.svg';
-import tigerColor from './../assets/img/tiger-color.png';
-import tigerNotColor from './../assets/img/tiger-not-color.svg';
-import zebraColor from './../assets/img/zebra-color.svg';
-import zebraNotColor from './../assets/img/zebra-not-color.svg';
-import ladybugColor from './../assets/img/ladybug-color.svg';
-import ladybugNotColor from './../assets/img/ladybug-not-color.svg';
-import nameZoo from './../assets/img/name-logo.png';
-import arrowBack from './../assets/img/arrow-back.svg';
+import zones from './zonas.json';
+import parrotColor from './assets/img/parrot-color.svg';
+import parrotNotColor from './assets/img/parrot-not-color.svg';
+import dolphinColor from './assets/img/dolphin-color.svg';
+import dolphinNotColor from './assets/img/dolphin-not-color.svg';
+import tigerColor from './assets/img/tiger-color.png';
+import tigerNotColor from './assets/img/tiger-not-color.svg';
+import zebraColor from './assets/img/zebra-color.svg';
+import zebraNotColor from './assets/img/zebra-not-color.svg';
+import ladybugColor from './assets/img/ladybug-color.svg';
+import ladybugNotColor from './assets/img/ladybug-not-color.svg';
+import nameZoo from './assets/img/name-logo.png';
+import arrowBack from './assets/img/arrow-back.svg';
 
 function App() {
   const [tableData, setTableData] = useState<Habitat[] | null>(null);
