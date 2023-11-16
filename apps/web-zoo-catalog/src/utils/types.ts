04763ab056453465053ec951dd271a522384d3e8
Fernando Guzman <fernandeitorc@gmail.com>
2023-11-16 14:56:44 -0400

    add type Zone

--- apps/web-zoo-catalog/src/utils/types.ts
+++ apps/web-zoo-catalog/src/utils/types.ts
@@ -1,14 +1,22 @@
-export interface Animal {
+export type ZoneName = 'acuario' | 'insectario' | 'carnivoros' | 'herbivoros' | 'aviario'
+
+export type Zone = {
+  zona: ZoneName;
+  habitats: Habitat[]
+}
+
+export type Habitat = {
+  id: number;
+  habitat: string;
+  cantidad: number;
+  animales: Animal[]
+}
+
+export type Animal = {
   codigoAnimal:string;
   fechaLlegada:string;
   estado: 'saludable'|'grave'|'regular';
   sexo:string;
 }
-export type Habitat = {
-      id: number;
-      habitat: string;
-      cantidad: number;
-      animales: Animal[]
-    }
-export type ZoneName = 'acuario' | 'insectario' | 'carnivoros' | 'herbivoros' | 'aviario'
+
 
