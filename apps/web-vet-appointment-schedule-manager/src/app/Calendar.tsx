80c82ac94c8947f4cc595a6bcc70ed757ccc5c5f
Adrian <amatatranspider6@gmail.com>
2023-12-05 16:46:05 -0400

    Code written entirely in typescript but with empty 'CalendarProps' interface warning

--- apps/web-vet-appointment-schedule-manager/src/app/Calendar.tsx
+++ apps/web-vet-appointment-schedule-manager/src/app/Calendar.tsx
@@ -5,17 +5,10 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import './calendar-styles.css';
 
-export function App() {
-  return (
-    <div className="calendar-page-container">
-      <Calendar />
-    </div>
-  );
-}
+interface CalendarProps {}
 
-export default function Calendar() {
+const Calendar: React.FC<CalendarProps> = () => {
   return (
-    
     <div className="calendar-container">
       <h2 className="calendar-header">Cronograma de Citas</h2>
       <FullCalendar
@@ -28,8 +21,9 @@ export default function Calendar() {
         }}
         contentHeight={'70vh'}
         height={750}
-        
       />
     </div>
   );
-}
+};
+
+export default Calendar;
